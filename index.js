f3cbf25c9048f070a45f872f34ebc80cdfec58ee
Joakim Beijar <jbeijar@mios.fi>
2014-10-26 11:27:35 +0200

    Rename

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var util = require("util");
 var Writable = require('stream').Writable;
 var winston = require("winston");
 
-util.inherits(ChunkStream, Writable);
-function ChunkStream(marker, streamFactory, options) {
+util.inherits(SplitStream, Writable);
+function SplitStream(marker, streamFactory, options) {
 	Writable.call(this, options);
 	this.streamFactory = streamFactory;
 	this.marker = marker;
@@ -13,17 +13,17 @@ function ChunkStream(marker, streamFactory, options) {
 		this._endStream();
 	});
 }
-ChunkStream.prototype._pushToStream = function(chunk) {
+SplitStream.prototype._pushToStream = function(chunk) {
 	if(!this.stream)
 		this.stream = this.streamFactory();
 	this.stream.write(chunk);
 }
-ChunkStream.prototype._endStream = function() {
+SplitStream.prototype._endStream = function() {
 	if(this.stream)
 		this.stream.end();
 	this.stream = null;
 }
-ChunkStream.prototype._write = function(chunk, _, callback) {
+SplitStream.prototype._write = function(chunk, _, callback) {
 	var p = 0; var last = 0;
 	while(p<chunk.length) {
 		if(chunk[p]===this.marker[this.matched])
